16232173812602248aaf66a0c65ed6c24405f722
wiinci <wiinci@users.noreply.github.com>
2018-11-11 17:56:25 -0500

    [chore] Tabs 4 → 2

--- src/App.vue
+++ src/App.vue
@@ -12,42 +12,41 @@ export default {
 
 <style lang="less">
 html {
-    font-family: BlinkMacSystemFont,-apple-system,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif;
-    font-size: 62.5%;
-    box-sizing: border-box;
+  font-family: BlinkMacSystemFont, -apple-system, "Segoe UI", Roboto,
+    Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+  font-size: 62.5%;
+  box-sizing: border-box;
 }
 * {
-    padding: 0;
-    margin: 0;
+  padding: 0;
+  margin: 0;
+  box-sizing: inherit;
+  &:before,
+  &:after {
     box-sizing: inherit;
-    &:before,
-    &:after {
-        box-sizing: inherit;
-    }
+  }
 }
 body {
-    font-size: 1.6rem;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    padding: 16px;
-    color: #453e45;
-    background: url("assets/images/1.png") #fafafa;
+  font-size: 1.6rem;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  padding: 16px;
+  color: #453e45;
+  background: url("assets/images/1.png") #fafafa;
 
-    @media only screen and (min-width: 501px) {
-        box-shadow:
-            inset 0 0 0 3px #fb7700,
-            inset 0 0 0 6px #fbc00d;
-        border: 3px solid #1e90d6;
-        padding: 28px;
-    }
+  @media only screen and (min-width: 501px) {
+    box-shadow: inset 0 0 0 3px #fb7700, inset 0 0 0 6px #fbc00d;
+    border: 3px solid #1e90d6;
+    padding: 28px;
+  }
 }
 body,
 html {
-    min-height: 100vh;
-    scroll-behavior: smooth;
-    -webkit-overflow-scrolling: touch;
+  min-height: 100vh;
+  scroll-behavior: smooth;
+  -webkit-overflow-scrolling: touch;
 }
 ::selection {
-    background-color: rgba(250, 179, 0, 0.3);
+  background-color: rgba(250, 179, 0, 0.3);
 }
 </style>
