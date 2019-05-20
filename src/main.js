3b2710a4bffcf3ec8f23fd67a839e16984099b49
wiinci <wiinci@users.noreply.github.com>
2019-05-19 20:14:21 -0400

    Lint files

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from "vue";
-import App from "./App";
-import router from "./router";
+import Vue from 'vue';
+import App from './App';
+import router from './router';
 
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
@@ -10,8 +10,8 @@ Vue.config.performance = true;
 
 /* eslint-disable no-new */
 new Vue({
-  el: "#app",
+  el: '#app',
   router,
   components: { App },
-  template: "<App/>",
+  template: '<App/>',
 });
