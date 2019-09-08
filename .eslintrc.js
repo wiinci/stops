bc65c695e5d1691a446cc45698f0789e4e270c08
wiinci <wiinci@users.noreply.github.com>
2019-09-08 16:55:27 -0400

    [update] Add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,92 +3,102 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint',
   },
   env: {
     browser: true,
   },
-  extends: ["plugin:vue/recommended", "airbnb-base"],
+  extends: ['plugin:vue/recommended', 'airbnb-base'],
   // required to lint *.vue files
-  plugins: [
-    "vue"
-  ],
+  plugins: ['vue'],
   // check if imports actually resolve
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       webpack: {
-        config: "build/webpack.base.conf.js"
-      }
-    }
+        config: 'build/webpack.base.conf.js',
+      },
+    },
   },
   // add your custom rules here
   rules: {
-    "arrow-parens": ["warn", "as-needed"],
-    "comma-dangle": ["warn", "only-multiline"],
-    "func-names": "off",
-    "global-require": "off",
-    "indent": ["warn", 2],
-    "max-len": "off",
-    "no-console": "off",
-    "no-mixed-operators": "off",
-    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
-    "no-undef": ["warn"],
-    "no-underscore-dangle": "off",
-    "object-curly-spacing": ["warn", "always"],
-    "prefer-arrow-callback": "off",
-    "quote-props": ["error", "consistent-as-needed"],
-    "quotes": ["error", "single", { avoidEscape: true }],
+    'arrow-parens': ['warn', 'as-needed'],
+    'comma-dangle': ['warn', 'only-multiline'],
+    'func-names': 'off',
+    'global-require': 'off',
+    indent: ['warn', 2],
+    'max-len': 'off',
+    'no-confusing-arrow': 'off',
+    'no-console': 'off',
+    'no-mixed-operators': 'off',
+    'no-nested-ternary': 'off',
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'no-undef': ['warn'],
+    'no-underscore-dangle': 'off',
+    'object-curly-spacing': ['warn', 'always'],
+    'prefer-arrow-callback': 'off',
+    'quote-props': ['error', 'consistent-as-needed'],
+    quotes: ['error', 'single', { avoidEscape: true }],
 
     // don"t require .vue extension when importing
-    "import/extensions": ["error", "always", {
-      js: "never",
-      vue: "never",
-      less: "never"
-    }],
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        vue: 'never',
+        less: 'never',
+      },
+    ],
     // disallow reassignment of function parameters
     // disallow parameter object manipulation except for specific exclusions
-    "no-param-reassign": ["error", {
-      props: true,
-      ignorePropertyModificationsFor: [
-        "state", // for vuex state
-        "acc", // for reduce accumulators
-        "e" // for e.returnvalue
-      ]
-    }],
+    'no-param-reassign': [
+      'error',
+      {
+        props: true,
+        ignorePropertyModificationsFor: [
+          'state', // for vuex state
+          'acc', // for reduce accumulators
+          'e', // for e.returnvalue
+        ],
+      },
+    ],
     // allow optionalDependencies
-    "import/no-extraneous-dependencies": ["error", {
-      optionalDependencies: ["test/unit/index.js"]
-    }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        optionalDependencies: ['test/unit/index.js'],
+      },
+    ],
     // allow debugger during development
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 
     // Vue settings
-    "vue/html-indent": [
-      "warn",
+    'vue/html-indent': [
+      'warn',
       2,
       {
         attribute: 1,
         closeBracket: 0,
         alignAttributesVertically: true,
-        ignores: []
-      }
+        ignores: [],
+      },
     ],
-    "vue/max-attributes-per-line": [
+    'vue/max-attributes-per-line': [
       2,
       {
         singleline: 1,
         multiline: {
           max: 1,
-          allowFirstLine: false
-        }
-      }
+          allowFirstLine: false,
+        },
+      },
     ],
-    "vue/html-closing-bracket-newline": [
-      "error",
+    'vue/html-closing-bracket-newline': [
+      'error',
       {
-        singleline: "never",
-        multiline: "always"
-      }
-    ]
-  }
-}
+        singleline: 'never',
+        multiline: 'always',
+      },
+    ],
+  },
+};
