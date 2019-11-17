dacb8b72b22d8de0f77225511017edb5f926723f
wiinci <wiinci@users.noreply.github.com>
2019-11-17 14:12:12 -0500

    [update] Add air quality APIs

--- src/api.js
+++ src/api.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
 
 const client = axios.create({ responseType: 'json' });
+
+const aqi = 'https://api.airvisual.com/v2/nearest_city';
 const stops = 'https://transit.land/api/v1/stops';
 const times = 'https://transit.land/api/v1/schedule_stop_pairs';
 const zip = 'https://public.opendatasoft.com/api/records/1.0/search/';
+
+const aqiParams = {
+  key: process.env.VUE_APP_AIRVISUAL_API_KEY,
+};
 
 const stopParams = {
   headway_dates: 'today',
@@ -31,6 +37,18 @@ export default {
     }).then(req => req.data);
   },
 
+  getAqi() {
+    return this.execute('get', aqi, { aqiParams });
+  },
+
+  getAqifromLatLon({ lat, lon }) {
+    return this.execute('get', aqi, { ...aqiParams, lat, lon });
+  },
+
+  getLatLonfromZip({ q }) {
+    return this.execute('get', zip, { ...zipParams, q });
+  },
+
   getStops({ lat, lon }) {
     return this.execute('get', stops, { ...stopParams, lat, lon });
   },
@@ -42,9 +60,5 @@ export default {
       operator_onestop_id,
       origin_onestop_id,
     });
-  },
-
-  getLatLonfromZip({ q }) {
-    return this.execute('get', zip, { ...zipParams, q });
   },
 };
