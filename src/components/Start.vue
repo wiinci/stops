8707e3591fe34b62aa792abdce2f18d45d5c19c0
wiinci <wiinci@users.noreply.github.com>
2019-12-29 12:21:25 -0500

    [update] Better form UI

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -4,28 +4,38 @@
     <form>
       <fieldset>
         <legend>Find using zip code</legend>
-        <label for="zip">Enter zip code</label>
-        <input
-          id="zip"
-          v-model="zip"
-          name="zip"
-          type="number"
-        />
-        <button
-          type="submit"
-          @click.stop.prevent="submit"
-        >
-          Search
-        </button>
+        <div class="controls">
+          <label
+            for="zip"
+            class="hidden"
+          >Enter zip code</label>
+          <input
+            id="zip"
+            v-model="zip"
+            name="zip"
+            type="number"
+          />
+          <button
+            type="submit"
+            @click.stop.prevent="submit"
+          >
+            Search
+          </button>
+        </div>
       </fieldset>
+      <p class="or">
+        or
+      </p>
       <fieldset>
         <legend>Find using location</legend>
-        <button
-          type="button"
-          @click.stop.prevent="gps"
-        >
-          Search
-        </button>
+        <div class="controls">
+          <button
+            type="button"
+            @click.stop.prevent="gps"
+          >
+            Search
+          </button>
+        </div>
       </fieldset>
     </form>
   </main>
@@ -37,10 +47,16 @@ export default {
   data() {
     return {
       zip: null,
+      isZipEmpty: false,
     };
   },
   methods: {
     submit() {
+      if (!this.zip) {
+        this.isZipEmpty = true;
+        return;
+      }
+      this.isZipEmpty = false;
       this.$router.push(`/zip/${this.zip}`);
     },
     gps() {
@@ -55,35 +71,85 @@ export default {
 @import (reference) '../assets/styles/bundles/typography.less';
 @import (reference) '../assets/styles/bundles/colors.less';
 
+form {
+  width: 40ch;
+}
+
 h1 {
   #type.h3();
-  margin-bottom: @base-unit * 3;
+  margin-bottom: @base-unit * 7;
 }
 
 fieldset {
   border: 0 none;
 
   + fieldset {
-    margin-top: @base-unit * 5;
+    margin-top: @base-unit * 10;
   }
 
   [type="number"] {
-    #type.h2();
+    #type.h3();
     #type.monospace();
     background: transparent;
+    border-radius: @base-unit;
     border: 0 none;
-    box-shadow: 0 0 0 4px rgba(255, 255, 255, 0.1);
+    box-shadow: 0 0 0 @base-unit #color.brand[blue];
     color: #color.text[light];
-    font-weight: 600;
-    height: @base-unit * 10;
+    font-weight: 300;
+    margin-bottom: @base-unit * 5;
     outline: 0;
-    padding: @base-unit;
-    width: 10ch;
+    padding: (@base-unit * 2);
 
     &:focus,
     &:hover {
-      box-shadow: 0 0 0 4px rgba(255, 255, 255, 0.5);
+      box-shadow: 0 0 0 @base-unit darken(#color.brand[blue], 5%);
     }
   }
+}
+
+legend {
+  margin-bottom: @base-unit * 2;
+}
+
+.controls {
+  display: flex;
+  flex-flow: column;
+}
+
+button {
+  #type.body();
+  #type.monospace();
+  background: #color.brand[blue];
+  border-radius: @base-unit;
+  border: 0 none;
+  box-shadow: 0 0 0 @base-unit #color.brand[blue];
+  color: contrast(#color.brand[blue], #color.text[dark], #color.text[light]);
+  padding: (@base-unit * 2) 0;
+
+  &:active,
+  &:focus,
+  &:hover {
+    background: darken(#color.brand[blue], 5%);
+    box-shadow: 0 0 0 @base-unit darken(#color.brand[blue], 5%);
+  }
+}
+
+h1,
+.or {
+  text-align: center;
+}
+
+.or {
+  #type.h3();
+  margin: (@base-unit * 7) auto;
+}
+
+.hidden {
+  position: absolute !important;
+    height: 1px;
+    width: 1px;
+    overflow: hidden;
+    clip: rect(1px, 1px, 1px, 1px);
+    white-space: nowrap;
 }
 </style>
